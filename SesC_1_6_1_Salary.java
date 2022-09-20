bf919948e7699640335f5295be75428aa5006dd9
Breanne Gross <71997507+bgrossdesign@users.noreply.github.com>
2022-09-20 18:54:52 -0400

    Add files via upload

---
+++ SesC_1_6_1_Salary.java
@@ -0,0 +1,14 @@
+package com.company.CSE110_SesC_Ch1;
+
+public class SesC_1_6_1_Salary
+{
+    public static void main (String[] args)
+    {
+        int wage;
+
+        wage = 35;
+
+        System.out.print("Salary is ");
+        System.out.println(wage * 50 *52);
+    }
+}
